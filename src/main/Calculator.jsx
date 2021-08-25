1dbc9a438743a48d2668bfb4f78a3d1940daa067
hugolfsimoes <hugolfsimoes@gmail.com>
2021-08-25 12:11:22 -0300

    feito os botoes da calculadora

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Button from "../Components/Button";
+import Display from "../Components/Display";
 import "./Calculator.css";
 
 export default class Calculator extends Component {
   render() {
     return (
     <div className="calculator">
+      <Display value={100}/>
       <Button label="AC"/>
       <Button label="/"/>
       <Button label="7"/>

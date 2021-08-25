8daa8c03fb0bb462ce28539e3cc63d8bf19a9787
hugolfsimoes <hugolfsimoes@gmail.com>
2021-08-25 11:57:24 -0300

    inicindo o display

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
+import Button from "../Components/Button";
 import "./Calculator.css";
 
 export default class Calculator extends Component {
   render() {
     return (
     <div className="calculator">
-      <p>oi</p>
+      <Button label="AC"/>
+      <Button label="/"/>
+      <Button label="7"/>
+      <Button label="AC"/>
+      <Button label="AC"/>
+      <Button label="AC"/>
+      <Button label="AC"/>
+      <Button label="AC"/>
+      <Button label="AC"/>
+      <Button label="AC"/>
+      <Button label="AC"/>
+      <Button label="AC"/>
+      <Button label="AC"/>
+      <Button label="AC"/>
+      <Button label="AC"/>
+      <Button label="AC"/>
+      <Button label="AC"/>
     </div>)
   }
 }

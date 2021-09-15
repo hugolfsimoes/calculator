b74589dc54a11f95fe8af7bc60983d1774a0f27a
hugolfsimoes <hugolfsimoes@gmail.com>
2021-09-15 10:19:31 -0300

    feito o designer dos botões e os eventos de click

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import './Button.css';
 
 export default function Button(props) {
+  let classes = 'button '
+  classes += props.operation? 'operation': ''
+  classes += props.double? 'double': ''
+  classes += props.triple? 'triple': ''
+  
   return(
-    <button className={`
-    button 
-    ${props.operation? 'operation' : ''}
-    ${props.double? 'double' : ''}
-    ${props.triple? 'triple' : ''}
-
-    `}>
+    <button
+    onClick={e => props.click(props.label)} 
+    className={classes}>
       {props.label}
     </button>
   )
